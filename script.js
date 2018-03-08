ae76154f1fe636a9eb7a0e104e07b2aa6d7aabab
jaderosse <jademrosse@gmail.com>
2018-03-08 11:26:22 -0800

    pics chosen, links working. need to adjust for mobile view otherwise done

--- script.js
+++ script.js
@@ -10,27 +10,53 @@ $(document).ready( function() {
     	cursor: false
 	})
 	.type('Full-Stack Web Developer...')
-	.pause(2000)
-	.options({speed: 300})
+	.pause(500)
+	.options({speed: 50})
 	.delete(3)
 	.options({speed: 100})
-	.pause(1000)
+	.pause(500)
 	.type('<p>& Musician & Bibliophile & Dog Mom</p>')
 	.pause(1000)
 	.type('<h2>&hearts;</h2>')
 
 	new TypeIt('#about-type', {
 		strings: 'About Me',
-		speed: 100,
+		speed: 50,
 		lifeLike: true,
 		autoStart: false
 	});
 
 	new TypeIt('#project-type', {
 		strings: 'Projects',
-		speed: 100,
+		speed: 50,
 		lifeLike: true,
 		autoStart: false
 	});
+
+	$("#gold").hover(function(){
+		$("#gold img").hide();
+		$("#gold a").show();
+	}, function(){
+		$("#gold img").show();
+		$("#gold a").hide();
+	});
+
+	$("#blues").hover(function(){
+		$("#blues img").hide();
+		$("#blues a").show();
+	}, function(){
+		$("#blues img").show();
+		$("#blues a").hide();
+	});
+
+
+	$("#res").hover(function(){
+		$("#res i").hide();
+		$("#res a").show();
+	}, function(){
+		$("#res i").show();
+		$("#res a").hide();
+	});
+
 
 });
